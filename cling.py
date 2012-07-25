370253dc4bc1dd5f90dff8b435a5c8cd24eeec10
Mark Henderson <emehrkay@gmail.com>
2012-07-24 23:29:57 -0400

    external config wasnt quite working

--- cling.py
+++ cling.py
@@ -10,8 +10,13 @@ import os.path
 
 from tornado import httpserver, ioloop, web
 from tornado.options import define, options
-import config
 from utils import parse_page, directory_listing, slug_to_name
+
+define('page_dir', default=os.path.join(os.path.dirname(__file__), 'page'), 
+    help='The directory where the page.md files are stored')
+define('page_404', default=os.path.join(os.path.dirname(__file__), 'page', '404.md'), 
+    help='The path to the 404.md page')
+define('sort_article_by', default='modified', help='Parameter to sort articles by; modifed or created')
 
 class Application(web.Application):
     """main appliction launcher"""
